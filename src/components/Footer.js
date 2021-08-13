f3cdb2f6a9d3019907c46571fb172288c8528266
surmount96 <ayanwoye74@gmail.com>
2021-08-13 08:00:58 +0100

    footer styling cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,16 @@ const Footer = () => {
     return(
         <footer className="footer">
             <p className="footer-header">Nigeria</p>
-            <p className="text-center footer-14 pt-2 pb-1">Carbon neutral since 2007</p>
-            <div className="container flex justify-between footer-14">
-                <ul className="flex lg:flex-no-wrap md:flex-no-wrap flex-wrap">
+            <p className="container text-center footer-14 pt-2 pb-1">Carbon neutral since 2007</p>
+            <div className="container flex lg:justify-between justify-center lg:flex-no-wrap md:flex-no-wrap flex-wrap footer-14">
+                <ul className="flex lg:flex-no-wrap md:flex-no-wrap flex-wrap lg:my-0 md:my-0 my-4">
                     <li>About</li>
-                    <li className="px-4">Advertising</li>
-                    <li className="px-4">Business</li>
-                    <li>How Search Works</li>
+                    <li className="px-5">Advertising</li>
+                    <li className="p">Business</li>
                 </ul>
-                <ul className="flex">
+                <ul className="flex lg:flex-no-wrap md:flex-no-wrap flex-wrap">
                     <li>Privacy</li>
-                    <li className="px-4">Terms</li>
+                    <li className="px-5">Terms</li>
                     <li>Settings</li>
                 </ul>
             </div>
